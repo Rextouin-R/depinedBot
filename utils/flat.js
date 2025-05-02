1d6291d9ee686435a9f62aa45287ac36be9bcd03
Rextouin-R <rattatouille1994@gmail.com>
2025-05-02 16:24:23 +0700

    Update flat.js

--- utils/flat.js
+++ utils/flat.js
@@ -1,1 +1,34 @@
+import chalk from 'chalk';
 
+const flat = {
+    log: (level, message, value = '') => {
+        const now = new Date().toLocaleString();
+
+        const colors = {
+            strip: chalk.white,
+        };
+
+        const color = colors[level] || chalk.white;
+        const levelTag = `[ ${level.toUpperCase()} ]`;
+        const timestamp = `[ ${now} ]`;
+
+        const formattedMessage = `${chalk.white("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")}`;
+
+        let formattedValue = ` ${chalk.green(value)}`;
+        if (level === 'error') {
+            formattedValue = ` ${chalk.red(value)}`;
+        } else if (level === 'warn') {
+            formattedValue = ` ${chalk.yellow(value)}`;
+        }
+        if (typeof value === 'object') {
+            const valueColor = level === 'error' ? chalk.red : chalk.green;
+            formattedValue = ` ${valueColor(JSON.stringify(value))}`;
+        }
+
+        console.log(`${formattedMessage}${formattedValue}`);
+    },
+
+    strip: (message, value = '') => flat.log('strip', message, value),
+};
+
+export default flat;
