a871fdf46243ba2d9175c476fc2bfced8f145eb6
Rextouin-R <rattatouille1994@gmail.com>
2025-05-02 20:55:52 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,86 +1,83 @@
-import * as utils from './utils/api.js';
+import { saveToFile, delay, readFile } from './utils/helper.js';
+import log from './utils/logger.js'
+import Mailjs from '@cemalgnlts/mailjs';
 import banner from './utils/banner.js';
-import log from './utils/logger.js';
-import { readFile, delay } from './utils/helper.js'
+
+import {
+    registerUser,
+    createUserProfile,
+    confirmUserReff,
+    getUserRef
+} from './utils/api.js'
+const mailjs = new Mailjs();
 
 const main = async () => {
     log.info(banner);
-    await delay(3)
-    const tokens = await readFile("tokens.txt");
-    if (tokens.length === 0) {
-        log.error('No tokens found in tokens.txt');
-        return;
-    }
-    const proxies = await readFile("proxy.txt");
-    if (proxies.length === 0) {
-        log.warn('Running without proxy...');
-    }
+    log.info(`proccesing run auto register (CTRL + C to exit)`);
+    await delay(3);
+    const tokens = await readFile("tokens.txt")
+    for (let i = 0; i < 5; i++) {
+        for (const token of tokens) {
+            const response = await getUserRef(token);
+            if (!response?.data?.is_referral_active) continue;
+            const reffCode = response?.data?.referral_code;
+            if (reffCode) {
+                log.info(`Found new active referral code:`, reffCode);
+                try {
+                    let account = await mailjs.createOneAccount();
+                    while (!account?.data?.username) {
+                        log.warn('Failed To Generate New Email, Retrying...');
+                        await delay(3)
+                        account = await mailjs.createOneAccount();
+                    }
 
-    try {
-        log.info(`Starting Program for all accounts:`, tokens.length);
+                    const email = account.data.username;
+                    const password = account.data.password;
 
-        for (let index = 0; index < tokens.length; index++) {
-            const token = tokens[index]
-            const proxy = proxies[index % proxies.length] || null;
-            try {
-                const userData = await utils.getUserInfo(token, proxy);
+                    log.info(`Trying to register email: ${email}`);
+                    let regResponse = await registerUser(email, password, null);
+                    while (!regResponse?.data?.token) {
+                        log.warn('Failed To Register, Retrying...');
+                        await delay(3)
+                        regResponse = await registerUser(email, password, null);
+                    }
+                    const token = regResponse.data.token;
 
-                if (userData?.data) {
-                    const { email, verified, current_tier, points_balance } = userData.data
-                    log.info(`Account ${index + 1} info:`, { email, verified, current_tier, points_balance });
+                    log.info(`Trying to create profile for ${email}`);
+                    await createUserProfile(token, { step: 'username', username: email });
+                    await createUserProfile(token, { step: 'description', description: "AI Startup" });
+
+
+                    let confirm = await confirmUserReff(token, reffCode);
+                    while (!confirm?.data?.token) {
+                        log.warn('Failed To Confirm Referral, Retrying...');
+                        await delay(3)
+                        confirm = await confirmUserReff(token, reffCode);
+                    }
+
+                    await saveToFile("accounts.txt", `${email}|${password}`)
+                    await saveToFile("tokens.txt", `${confirm.data.token}`)
+
+                } catch (err) {
+                    log.error('Error creating account:', err.message);
                 }
-
-                await checkUserRewards(token, proxy);
-
-                setInterval(async () => {
-                    const connectRes = await utils.connect(token, proxy);
-                    log.info(`Ping result for account ${index + 1}:`, connectRes || { message: 'unknown error' });
-
-                    const result = await utils.getEarnings(token, proxy);
-                    log.info(`Earnings result for account ${index + 1}:`, result?.data || { message: 'unknown error' });
-                }, 1000 * 30); // Run every 30 seconds
-
-                setInterval(async () => {
-                    await checkUserRewards(token, proxy);
-                }, 1000 * 60 * 60 * 24); // check every 24 hours
-
-            } catch (error) {
-                log.error(`Error processing account ${index}: ${error.message}`);
+            } else {
+                log.warn('No referral code found for this account');
             }
         };
-
-        await Promise.all(accountsProcessing);
-    } catch (error) {
-        log.error(`Error in main loop: ${error.message}`);
     }
 };
 
-const checkUserRewards = async (token, proxy) => {
-    try {
-        const response = await utils.getUserRef(token, proxy)
-        const { total_unclaimed_points } = response?.data || 0;
-        if (total_unclaimed_points > 0) {
-            log.info(`Account ${index + 1} has ${total_unclaimed_points} unclaimed points, trying to claim it...`);
-            const claimResponse = await utils.claimPoints(token, proxy);
-            if (claimResponse.code === 200) {
-                log.info(`Account ${index + 1} claimed successfully! ${total_unclaimed_points} points`);
-            }
-        }
-    } catch (error) {
-        log.error(`Error checking user rewards: ${error.message}`);
-    }
-}
-
+// Handle CTRL+C (SIGINT)
 process.on('SIGINT', () => {
-    log.warn(`Process received SIGINT, cleaning up and exiting program...`);
-    process.exit(0);
+    log.warn('SIGINT received. Exiting...');
+    process.exit();
 });
 
-process.on('SIGTERM', () => {
-    log.warn(`Process received SIGTERM, cleaning up and exiting program...`);
-    process.exit(0);
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+    log.error('Uncaught exception:', err);
+    process.exit(1);
 });
 
-
-// Run the main function
 main();
