73ad236d55ad31b73777c2a3ccdf9012a7149dc9
Rextouin-R <rattatouille1994@gmail.com>
2025-05-02 20:56:35 +0700

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -1,46 +1,83 @@
-import { HttpsProxyAgent } from 'https-proxy-agent';
-import { SocksProxyAgent } from 'socks-proxy-agent';
-import fs from 'fs/promises';
-import log from './logger.js';
+import { saveToFile, delay, readFile } from './utils/helper.js';
+import log from './utils/logger.js'
+import Mailjs from '@cemalgnlts/mailjs';
+import banner from './utils/banner.js';
 
-export function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms * 1000));
-}
+import {
+    registerUser,
+    createUserProfile,
+    confirmUserReff,
+    getUserRef
+} from './utils/api.js'
+const mailjs = new Mailjs();
 
-// Save data to a file
-export async function saveToFile(filename, data) {
-    try {
-        await fs.appendFile(filename, `${data}\n`, 'utf-8');
-        log.info(`Data saved to ${filename}`);
-    } catch (error) {
-        log.error(`Failed to save data to ${filename}: ${error.message}`);
+const main = async () => {
+    log.info(banner);
+    log.info(`proccesing run auto register (CTRL + C to exit)`);
+    await delay(3);
+    const tokens = await readFile("tokens.txt")
+    for (let i = 0; i < 5; i++) {
+        for (const token of tokens) {
+            const response = await getUserRef(token);
+            if (!response?.data?.is_referral_active) continue;
+            const reffCode = response?.data?.referral_code;
+            if (reffCode) {
+                log.info(`Found new active referral code:`, reffCode);
+                try {
+                    let account = await mailjs.createOneAccount();
+                    while (!account?.data?.username) {
+                        log.warn('Failed To Generate New Email, Retrying...');
+                        await delay(3)
+                        account = await mailjs.createOneAccount();
+                    }
+
+                    const email = account.data.username;
+                    const password = account.data.password;
+
+                    log.info(`Trying to register email: ${email}`);
+                    let regResponse = await registerUser(email, password, null);
+                    while (!regResponse?.data?.token) {
+                        log.warn('Failed To Register, Retrying...');
+                        await delay(3)
+                        regResponse = await registerUser(email, password, null);
+                    }
+                    const token = regResponse.data.token;
+
+                    log.info(`Trying to create profile for ${email}`);
+                    await createUserProfile(token, { step: 'username', username: email });
+                    await createUserProfile(token, { step: 'description', description: "AI Startup" });
+
+
+                    let confirm = await confirmUserReff(token, reffCode);
+                    while (!confirm?.data?.token) {
+                        log.warn('Failed To Confirm Referral, Retrying...');
+                        await delay(3)
+                        confirm = await confirmUserReff(token, reffCode);
+                    }
+
+                    await saveToFile("accounts.txt", `${email}|${password}`)
+                    await saveToFile("tokens.txt", `${confirm.data.token}`)
+
+                } catch (err) {
+                    log.error('Error creating account:', err.message);
+                }
+            } else {
+                log.warn('No referral code found for this account');
+            }
+        };
     }
-}
-
-// Read the file
-export async function readFile(pathFile) {
-    try {
-        const datas = await fs.readFile(pathFile, 'utf8');
-        return datas.split('\n')
-            .map(data => data.trim())
-            .filter(data => data.length > 0);
-    } catch (error) {
-        log.error(`Error reading file: ${error.message}`);
-        return [];
-    }
-}
-
-// Create an agent
-export const newAgent = (proxy = null) => {
-    if (proxy) {
-        if (proxy.startsWith('http://')) {
-            return new HttpsProxyAgent(proxy);
-        } else if (proxy.startsWith('socks4://') || proxy.startsWith('socks5://')) {
-            return new SocksProxyAgent(proxy);
-        } else {
-            log.warn(`Unsupported proxy type: ${proxy}`);
-            return null;
-        }
-    }
-    return null;
 };
+
+// Handle CTRL+C (SIGINT)
+process.on('SIGINT', () => {
+    log.warn('SIGINT received. Exiting...');
+    process.exit();
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+    log.error('Uncaught exception:', err);
+    process.exit(1);
+});
+
+main();
