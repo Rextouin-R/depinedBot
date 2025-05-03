531bf66f817d94c631f33555862c35bbb995ed91
Rextouin-R <rattatouille1994@gmail.com>
2025-05-04 01:35:07 +0700

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -11,21 +11,12 @@ const Colors = {
 };
 
 const banner = `
-   ▄▀█ █ █▀█ █▀▄ █▀█ █▀█ █▀█ ∞ ${Colors.Gold}
-   █▀█ █ █▀▄ █▄▀ █▀▄ █▄█ █▀▀ ${Colors.Gold}
-   ┏━┓ ┏━┓         ┏━┓ ╔═╗             ╔═╗ ┏━┓__            ┏━┓${Colors.Green}
-   ┃ ┃ ┃ ┃ ┏━╻━━━┓ ┃ ┃ ┏━┓ ┏━╻━━╻━━━━┓ ┏━┓ ┃ ┏━┛  ┏━━━━╮ ╭━━╹ ┃${Colors.Blue}
-   ┃ ┗━┛ ┃ ┃ ┏━┓ ┃ ┃ ┃ ┃ ┃ ┃ ┏━┓ ┏━┓ ┃ ┃ ┃ ┃ ┗━━┓ ┃ ┏━━┛ ┃ ━━ ┃${Colors.Teal}
-   ┗━━━ ━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━┛ ┗━━━━┛ ┗━━━━┛ ╰━━━━┛
-    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${Colors.Teal}{${Colors.Neon}Rextouin${Colors.Teal}}${Colors.RESET}
-
-  \n${Colors.RESET}DEPINED Bot ${Colors.Blue}{ ${Colors.Neon}JS${Colors.Blue} }${Colors.RESET}
-    \n${Colors.Green}${'―'.repeat(50)}
-    \n${Colors.Gold}[+]${Colors.RESET} JOIN : ${Colors.Teal}==> 🟦 join channel : https://t.me/UNLXairdop
-    \n${Colors.Gold}[+]${Colors.RESET} FOLLOW : ${Colors.Teal}==> ⬛ github : https://github.com/Rextouin-R/
-    \n${Colors.Green}${'―'.repeat(50)}
-    \n${Colors.Gold}]-> ${Colors.Blue}{ ${Colors.RESET}DEPINED Extension${Colors.Neon} v1.1.4${Colors.Blue} } ${Colors.RESET}
-    \n${Colors.Gold}]-> ${Colors.Blue}{ ${Colors.RESET}BOT${Colors.Neon} v1.0.0${Colors.Blue} } ${Colors.RESET}
-    \n${Colors.Green}${'―'.repeat(50)}
+  8
+924853732
+8
+8
+iStock
+Credit: PeterPal
+8
                  `
 export default banner;
