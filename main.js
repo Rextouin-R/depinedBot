ccf2375286f0c8df0ea29e149bcbecbc72142e95
Rextouin-R <rattatouille1994@gmail.com>
2025-05-03 20:45:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import banner from './utils/banner.js';
 import log from './utils/logger.js';
 import { readFile, delay } from './utils/helper.js'
 
+const Colors = {
+  Gold: "\x1b[38;5;220m",
+  Red: "\x1b[31m",
+  Teal: "\x1b[38;5;51m",
+  Green: "\x1b[32m",
+  Neon: "\x1b[38;5;198m",
+  Blue: "\x1b[34m",
+  Magenta: "\x1b[95m",
+  Dim: "\x1b[2m",
+  RESET: "\x1b[0m"
+};
+
 const main = async () => {
     log.info(banner);
     await delay(3)
