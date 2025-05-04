592820e1f3960aa5e4aef8b438dca0516d242e4f
Rextouin-R <rattatouille1994@gmail.com>
2025-05-04 22:05:26 +0700

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -11,31 +11,33 @@ const Colors = {
 };
 
 const banner = `
-         ,c.                                                                    .c,                 
-cc. .;.  .lxl'                               ''                               'cxl.  .;' .:c.       
-.oOxxKKdc,'oXN0d:'.                     'clokXk.                         .':oOXXo',cd0KxxOd'        
-  ;0WMMMMWX0XWMMMN0kocc:'.            'dOXXXWMNo.                 .':cclx0NWMMMN0KWMMMMW0:          
-   'kNWMMMMMMMMMMMMMMMMWNKOkxdol:'    .,'c0WMMMNd.        ':loddxO0NWMMMMMMMMMMMMMMMMMNk,           
-    'dKWMMMMMMMMMMMMMMMMMMMMMMMMMNk;.     lNMMMMWo'.    ;xNMMMMMMMMMMMMMMMMMMMMMMMMMWXd'            
-      ;kNWMMMMMMMMMMMMMMMMMMMMMMMMMW0o,. .xMMMMMMNKc.,lONMMMMMMMMMMMMMMMMMMMMMMMMMWNO:.             
-       .l0WMMMMMMMMMMMMMMMMMMMMMMMMMNK0kod0XXkooooo:;ckNMMMMMMMMMMMMMMMMMMMMMMMMMWKo.               
-        .c0NMMMMMMMMMMMMMMMMMMMMMMMMXkkXWWX0Kl .codoc.,0MMMMMMMMMMMMMMMMMMMMMMMMW0l.                
-          ;xXWMMMMMMMMMMMMMMMMMMMMMMXkoc:;co0l.ckKMN0:;0MMMMMMMMMMMMMMMMMMMMMMWNk:.                 
-           .;d0WMMMMMMMMMMMMMMMMMMMMX0Xk,.lKXddk::dc. '0MMMMMMMMMMMMMMMMMMMMWKx:.                   
-              .,o0NWMMMMMMMMMMMMMMMMXocl;;d0c.:0kllcccoKMMMMMMMMMMMMMMMMWN0d;.                      
-                 .,oOKWMMMMMMMMMMMMMWl';;:lkkdk0kxOOxokNMMMMMMMMMMMMMWXOd,.                         
-                     .;cokKNNWMMMMMMM0;,cccodOXxdKNKooKMMMMMMMWNNKkoc:.                             
-                          .,:okOO0KXWMKl;,;,.cKkxkddxXWWXK0OOko:,.                                  
-                               ...'xWMMW0dc'.:KKO0KXWMMMk,....                                      
-                          ..      .OMMMNNWWX00XNWWWNNMMM0'       ..                                 
-                        .cOKo'    '0Xxl:kWMMMMMMMWW0ccd0Kc    .;xKx;.                               
-                         .:kNXOkddKK;  :XMMMMMMMMMMWo. .dXkoxkONXd,                                 
-                            ,:oXWMMWx,:0MMMMMMMMMMMMNo'c0WMMWkc:'                                   
-                               .;:lx0XNMMMMMMMMMMMMMMWXKOdc:,.                                      
-                                    ;KMMMMMMMMMMMMMMWMNo.                                           
-                                   .lk00xOOOK0KOOOxx00kd'                                           
-                 
-                 
+.          .l:.                                                                                 'o:          ..         
+cxc..,l;.   ,kOo;.                                   .;c.                                    'cxOo.   .cc..,ox,         
+ :OKkx0X0xc,.,kNWKkl;.                          .:odx0NX:                               .':dONWXl.':okXNkx00d'          
+  .dXWMMMMMWX00NMMMMWX0xl;,;'.                ,dKWN0KWMWO;                       .;,,cokKNMMMMWXO0NWMMMMMW0:            
+   .cOXWMMMMMMMMMMMMMMMMMWNNNKkdlcc:;,'.      ;o:oKNMMMMMK;            .';:ccloxOXNNWMMMMMMMMMMMMMMMMMMNKd,             
+    .;ONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKk:.      .,kMMMMMM0;        .oOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd.              
+      .dXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWOc.     'OMMMMMMM0o,   .,dXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN0c.               
+       .ckXNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXkc'..:XMMMMMMMWWx,;o0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWN0d,                 
+         .lONMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMX0X0xxOKXXklcccclc::cxNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXx:.                  
+          .c0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0x0NWWMX0Nd. .:lol:' ;XMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXx,                    
+           .cONMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0ocooldllKd.'kKWMWWK;;KMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXd,                     
+             .l0WMMMMMMMMMMMMMMMMMMMMMMMMMMM0O0l. .c0Xkclo:xNXo'.;KMMMMMMMMMMMMMMMMMMMMMMMMMMMNx;.                      
+               .,lkXNWMMMMMMMMMMMMMMMMMMMMMM0kXKo,lOKkco00c';,.. :XMMMMMMMMMMMMMMMMMMMMMMWNKd:'.                        
+                   .ck0NWMMMMMMMMMMMMMMMMMMMk,;;,:;dk,..c0OxxdlooxXMMMMMMMMMMMMMMMMMMMWXOd;.                            
+                      .cx0XWMMMMMMMMMMMMMMMMK;,c;lllOOOO00ddxkklcOWMMMMMMMMMMMMMMMMNKOd,.                               
+                         .,:cdO0NMWWMMMMMMMMWx';c:clddkX0odXMNkcdXMMMMMMMMWWWWXOkoc:.                                   
+                               .:cok0XNKKNWWMWk;,::::';KkckOxolxXMMWWXKXNKOxl:,.                                        
+                                   ..,''::dNMMMXxc;.  ,KXkddxkKWMMM0l:;','..                                            
+                                         .xMMMMMMWKxo:l0KKKNWMMMMMMX:                                                   
+                             ..,c'       .xWMMW00NMMMMWWMMMMMX0XMMMNc        .c;..                                      
+                             'ckNXx,     'OXxc;'dWMMMMMMMMMMMXc':ckXk.   . 'dXNOc,                                      
+                               'oKWN00koxXXl   :XMMMMMMMMMMMMM0'  .lXKdok00XMXd,                                        
+                                 .lldXMMMMWk,.'0MMMMMMMMMMMMMMWk. 'xWMMMMNdlo'                                          
+                                     ,ldx0NWX0KWMMMMMMMMMMMMMMMWKOXWN0xdo;                                              
+                                          .;xNMMMMMMMMMMMMMMMMMMMNx:'.                                                  
+                                           .kWMMMMMMMMMMMMMMMMMMMNo.                                                    
+                                          .:ddkkldkox0xkOoxxoldOxod,                             
                  
                  `
 export default banner;
