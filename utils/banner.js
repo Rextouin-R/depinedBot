9c1c39506b32773d581840e63d141e2a2584083b
Rextouin-R <rattatouille1994@gmail.com>
2025-05-06 03:39:06 +0700

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -26,8 +26,8 @@ const banner = `
              .:clKWMMMMMMMWc . .lc.   'lldOOxddOXMWXO,     :NMMkoOXMX:       .dx:.c00KKKk;  :KMMMWKxkXMMMMMMMMMMMo             
                . .cONMMMMMK,                    'c;.       .lXMd.,dNMKl.      ,:' .''..;do.  'l;;oOXNWMMMMMMMMMMWl                
                     ;xNMMMK,                                ;XWo  .l0Kx.                          ..';coxKWMMMMMWl                
-                      'dxdl'.'''..                          ,dd,     .                           :d.     .oNMMMMWc                
-${Colors.Red};█▀█ █▀▀ █▀█ █░█ █▄▄ █░ █ █▄▀${Colors.RESET}; .l0NWWNKxc,'.,cc:,'..'.         .    .dx.      .xMMMMNc
+                      'dxdl'.'''..                          ,dd,     .                           :d.                   .oNMMMMWc                
+${Colors.Red};█▀█ █▀▀ █▀█ █░█ █▄▄ █░ █ █▄▀${Colors.RESET}; .l0NWWNKxc,'.,cc:,'..'.         .    .dx.                    .xMMMMNc
 ${Colors.Red};█▀▄ ██▄ █▀▀ █▄█ █▄█ █▄ █ █░█${Colors.RESET};  .'ckO0XNNNNNNWMMWNOooc. .;,   .. .  .ol.     .oXMMMMWc
                        ▄▀▄   █   █▀▀▄   █▀▄   █▀▀▄   ▄▀▄   █▀▄KXo;okl.,lol,..clc::::;:;'.,;:x0x,..       ;kOkdlxXl                
                        █▀█   █   █▐█▀   █░█   █▐█▀   █░█   █▄█.;'.':'.colcc:lxooooc:,..':dOkdl:. 
