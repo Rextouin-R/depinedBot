3ca4c528d6a85cea66a486a89741f1312b757dee
Rextouin-R <rattatouille1994@gmail.com>
2025-05-06 03:11:03 +0700

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -22,16 +22,18 @@ const banner = `
           .dXMMMMMMMM0'.c:        .xMMMMMMMMMMMMMMMMMMMK,    :kc;::;;;clc:.     .k0c'  .:c' ..                                    
           '',OWMMMMMMNkl:'  .      .xNMMMMMMMMMMMMMMMMXo.   .O0:.';codo;         ,l,    l0OkKKkdo'  ,l:.                          
           :d''xWMMMMMMMXc .o0c       oWMMMMMMMMMMMMMMX:    .xWMWNNWKd;.  'cc;;. .cc.   ,cld0WMMWMx. 'oxllxko:'.                   
-            .',xNMMMMMMMXkdcok,.,,.  '0MMMMMMMMMMMMMMk.    '0MMWNWW0:    .''''.  ....',:'  .dKNWWK, .cKWMMMMMNKOx,                
-             .:clKWMMMMMMMWc . .lc.   'lldOOxddOXMWXO,     :NMMkoOXMX:       .dx:.c00KKKk;  :KMMMWKxkXMMMMMMMMMMMo ${Colors.RESET};            
+            .',xNMMMMMMMXkdcok,.,,.  '0MMMMMMMMMMMMMMk.    '0MMWNWW0:    .''''.  ....',:'  .dKNWWK, .cKWMMMMMNKOx, ${Colors.RESET};               
+             .:clKWMMMMMMMWc . .lc.   'lldOOxddOXMWXO,     :NMMkoOXMX:       .dx:.c00KKKk;  :KMMMWKxkXMMMMMMMMMMMo             
                . .cONMMMMMK,                    'c;.       .lXMd.,dNMKl.      ,:' .''..;do.  'l;;oOXNWMMMMMMMMMMWl                
                     ;xNMMMK,                                ;XWo  .l0Kx.                          ..';coxKWMMMMMWl                
                       'dxdl'.'''..                          ,dd,     .                           :d.     .oNMMMMWc                
-                          .l0NWWNKxc,'.,cc:,'..'.                                          .    .dx.      .xMMMMNc                
-                           .'ckO0XNNNNNNWMMWNOooc.                           .;,   .. .  .ol.    ..      .oXMMMMWc                
-                                ..''',:okO000OOKXo;okl.,lol,..clc::::;:;'.,;:x0x,..   .. ;c.             ;kOkdlxXl                
-                                             ..';'.':'.colcc:lxooooc:,..':dOkdl:.                               ..                
-                                                           .cxOd'      c0Kx;                                                      
-                                                              .'.      .'.               
+█▀█ █▀▀ █▀█ █░█ █▄▄ █░ █ █▄▀ .l0NWWNKxc,'.,cc:,'..'.                                          .    .dx.      .xMMMMNc                
+█▀▄ ██▄ █▀▀ █▄█ █▄█ █▄ █ █░█  .'ckO0XNNNNNNWMMWNOooc.                           .;,   .. .  .ol.    ..      .oXMMMMWc                
+            ╔═══╦══╦═══╦═══╦═══╗╔═══╦═══╗..''',:okO000OOKXo;okl.,lol,..clc::::;:;'.,;:x0x,..   .. ;c.             ;kOkdlxXl                
+            ║╔═╗╠╣╠╣╔═╗╠╗╔╗║╔═╗║║╔═╗║╔═╗║   ..';'.':'.colcc:lxooooc:,..':dOkdl:.                               ..                
+            ║║─║║║║║╚═╝║║║║║╚═╝║║║─║║╚═╝║                   .cxOd'      c0Kx;                                                      
+            ║╔═╗╠╣╠╣║║╚╦╝╚╝║║║╚╗║║─║║╔══╝                     .'.      .'.   
+            ║╔═╗╠╣╠╣║║╚╦╝╚╝║║║╚╗║╚═╝║║───
+            ╚╝─╚╩══╩╝╚═╩═══╩╝╚═╝╚═══╩╝───
                  `
 export default banner;
